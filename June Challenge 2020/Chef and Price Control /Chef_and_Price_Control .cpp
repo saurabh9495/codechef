7b6ae5673ba2f5289724b9e4368f988be45b621e
saurabh9495 <saurabhkumar4829@gmail.com>
2020-06-05 15:43:40 +0530

    Coding Commits made on 5th June 2020

--- June Challenge 2020/Chef and Price Control /Chef_and_Price_Control .cpp
+++ June Challenge 2020/Chef and Price Control /Chef_and_Price_Control .cpp
@@ -2,31 +2,36 @@
 #include <vector>
 using namespace std;
 typedef long long int lli;
-int maian()
+int main()
 {
     lli testcases;
     cin >> testcases;
     for (lli i = 0; i < testcases; i++)
     {
-        lli n, k, val, sum = 0;
+        lli n, k, val, initial_sum = 0, updated_sum = 0;
+        cin >> n >> k;
         vector<lli> v;
         for (lli j = 0; j < n; j++)
         {
             cin >> val;
             v.push_back(val);
         }
+        for (auto x : v)
+        {
+            initial_sum += x;
+        }
         for (auto it = v.begin(); it != v.end(); it++)
         {
             if (*it > k)
             {
-                sum += k;
+                updated_sum += k;
             }
             else
             {
-                sum += *it;
+                updated_sum += *it;
             }
         }
-        cout << sum << endl;
+        cout << initial_sum - updated_sum << endl;
     }
     return 0;
 }

1dacb4d160311556f25275d3626f3e34fafbffab
saurabh9495 <saurabhkumar4829@gmail.com>
2020-06-05 15:20:16 +0530

    Coding Commits made on 5th June 2020

---
+++ push.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Coding Commits made on 5th June 2020"
+git push origin master
---
+++ template_vector.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef long long int lli;
+int main()
+{
+    lli testcases;
+    cin >> testcases;
+    for (lli i = 0; i < testcases; i++)
+    {
+        lli noe, val;
+        vector<lli> v;
+        cin >> noe;
+        for (lli j = 0; j < noe; j++)
+        {
+            cin >> val;
+            v.push_back(val);
+        }
+        //code
+    }
+    return 0;
+}

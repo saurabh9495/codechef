b1b0212256b4e015eb4acdcfe60b83f6572c7d8f
saurabh9495 <saurabhkumar4829@gmail.com>
2020-06-06 17:10:39 +0530

    Coding Commits made on 5th June 2020

--- June Challenge 2020/The Tom and Jerry Game/The_Tom_and_Jerry_Game.cpp
+++ June Challenge 2020/The Tom and Jerry Game/The_Tom_and_Jerry_Game.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 typedef long long int lli;
+
+bool isPerfectSquare(long double x)
+{
+    long double sr = sqrt(x);
+    return ((sr - floor(sr)) == 0);
+}
+
 int main()
 {
     lli testcases;
@@ -20,19 +28,20 @@ int main()
             for (lli j = ts; j > 1; j -= 2)
             {
                 lli tt = ts, jj = j;
-                while (jj > 1)
+                tt = tt / 2;
+                jj = jj / 2;
+                if (tt % 2 == 0 and jj % 2 == 0)
                 {
-                    tt = tt / 2;
-                    jj = jj / 2;
-                    if (tt % 2 == 0 and jj % 2 == 0)
-                    {
+                    if(isPerfectSquare(tt)){
+                        ctr++;
                         continue;
                     }
-                    else if (tt % 2 == 1 and jj % 2 == 1)
+                    lli ttt = tt / 2, jjj = jj / 2;
+                    if (ttt % 2 == 1 and jjj % 2 == 1)
                     {
                         break;
                     }
-                    else if (tt % 2 == 0 and jj % 2 == 1)
+                    else if (ttt % 2 == 0 and jjj % 2 == 1)
                     {
                         break;
                     }
@@ -41,6 +50,19 @@ int main()
                         ctr++;
                         break;
                     }
+                }
+                else if (tt % 2 == 1 and jj % 2 == 1)
+                {
+                    break;
+                }
+                else if (tt % 2 == 0 and jj % 2 == 1)
+                {
+                    break;
+                }
+                else
+                {
+                    ctr++;
+                    break;
                 }
             }
             cout << ctr << endl;

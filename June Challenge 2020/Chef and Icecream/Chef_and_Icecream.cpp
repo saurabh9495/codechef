674503472ccf08257ec12308d6d7378c25c62b10
saurabh9495 <saurabhkumar4829@gmail.com>
2020-06-06 06:34:24 +0530

    Coding Commits made on 5th June 2020

---
+++ June Challenge 2020/Chef and Icecream/Chef_and_Icecream.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// 5 5 15
+int main()
+{
+    int testcases;
+    cin >> testcases;
+    for (int i = 0; i < testcases; i++)
+    {
+        int n, val, j = 0, ctr5 = 0, ctr10 = 0, ctr15 = 0;
+        cin >> n;
+        vector<int> v;
+        for (j = 0; j < n; j++)
+        {
+            cin >> val;
+            v.push_back(val);
+        }
+        int temp,k=0;
+        for (auto it = v.begin(); it != v.end(); it++,k++)
+        {
+            temp = *it / 5;
+            if (temp == 1)
+            {
+                ctr5++;
+            }
+            else if (temp == 2)
+            {
+                ctr10++;
+                ctr5--;
+                if (ctr5 < 0)
+                {
+                    break;
+                }
+            }
+            else
+            {
+                ctr15++;
+                if (ctr10 > 0)
+                {
+                    ctr10--;
+                }
+                else if (ctr5 > 1)
+                {
+                    ctr5 -= -2;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        if (k == n)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}

4e0fdb81860f285ffa5dc2d779b95818a5155878
saurabh9495 <saurabhkumar4829@gmail.com>
2020-06-06 14:50:41 +0530

    Coding Commits made on 5th June 2020

--- June Challenge 2020/The Tom and Jerry Game/The_Tom_and_Jerry_Game.cpp
+++ June Challenge 2020/The Tom and Jerry Game/The_Tom_and_Jerry_Game.cpp
@@ -12,15 +12,37 @@ int main()
         cin >> ts;
         if (ts % 2 == 1)
         {
-            for (lli j = ts - 1; j > 1; j -= 2)
-            {
-                cout << j << " ";
-            }
-            cout << endl;
+            cout << ts / 2 << endl;
         }
         else
         {
-            cout << "should";
+            lli ctr=0;
+            for (lli j = ts-2; j > 1; j-=2)
+            {
+                lli tt = ts,jj = j;
+                while (jj > 1)
+                {
+                    tt = tt/2;
+                    jj = j/2;
+                    if(tt%2 ==0 and jj%2==0){
+                        continue;
+                    }
+                    else if (tt%2 == 1 and jj%2 == 1)
+                    {
+                        break;
+                    }
+                    else if (tt%2 == 0 and jj%2 == 1)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        ctr++;
+                        break;
+                    }
+                }               
+            }
+            cout << ctr << endl;
         }
     }
     return 0;
